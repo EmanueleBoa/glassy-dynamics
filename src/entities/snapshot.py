fd5ad8ed46eb180d2e48b87f7c109ee83750e9ff
EmanueleBoa <emanuele.boattini@gmail.com>
2024-11-18 18:45:20 +0100

    add methods to snapshot

--- src/entities/snapshot.py
+++ src/entities/snapshot.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 import numpy as np
 
@@ -7,8 +7,9 @@ from src.entities.particle import Particle
 
 class Snapshot:
     def __init__(self, box: np.ndarray, particles: List[Particle]):
-        self.box = box
-        self.particles = particles
+        self.box: np.ndarray = box
+        self.particles: List[Particle] = particles
+        self.available_species: List[str] = self.get_available_species()
 
     @classmethod
     def load(cls, file_path: str):
@@ -17,3 +18,17 @@ class Snapshot:
             box = np.array(file.readline().split(), dtype=float)
             particles = [Particle.init_from_line(line) for line in file.readlines()]
         return cls(box, particles)
+
+    def get_particles_coordinates(self, target_species: Optional[str] = None) -> np.ndarray:
+        if target_species is None:
+            return np.array([particle.coordinates for particle in self.particles])
+
+        return np.array([particle.coordinates for particle in self.particles if particle.species == target_species])
+
+    def get_available_species(self) -> List[str]:
+        return list(set([particle.species for particle in self.particles]))
+
+    def get_other_species(self, species: str) -> str:
+        if species == self.available_species[0]:
+            return self.available_species[1]
+        return self.available_species[0]

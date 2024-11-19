ac53c616089759026da7791b20accbb1e354ec67
EmanueleBoa <emanuele.boattini@gmail.com>
2024-11-20 00:21:25 +0100

    refactor device

--- src/train/device.py
+++ src/train/device.py
@@ -1,11 +1,23 @@
 import torch
 
 
-class AvailableDevice:
+class Device:
     @staticmethod
-    def get() -> torch.device:
+    def get_available_device() -> torch.device:
         if torch.cuda.is_available():
-            return torch.device('cuda')
+            return Device.get_cuda()
         if torch.backends.mps.is_available():
-            return torch.device('mps')
+            return Device.get_mps()
+        return Device.get_cpu()
+
+    @staticmethod
+    def get_cpu():
         return torch.device('cpu')
+
+    @staticmethod
+    def get_mps():
+        return torch.device('mps')
+
+    @staticmethod
+    def get_cuda():
+        return torch.device('cuda')
